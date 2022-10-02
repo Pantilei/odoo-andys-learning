a0b84a19efa9ed06a2b2a5e1a42048e7ca4c186c
pantilei ianulov <ianulov.pantilei@gmail.com>
2022-10-02 21:15:08 +0300

    [FEAT]: Update nginx configs

--- custom-addons/restaurant_learning/__manifest__.py
+++ custom-addons/restaurant_learning/__manifest__.py
@@ -15,7 +15,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'muk_web_theme'],
+    'depends': ['base', 'web', 'muk_web_theme', 'website_slides'],
 
     # always loaded
     'data': [
